5056b66af6e28df9a5aed9f452dcd020c563b1e2
Ian <Ian@MARYBTH-0519-PC>
2021-06-30 13:24:17 -0500

    Add project files.

---
+++ src/Components/App/App.js
@@ -0,0 +1,19 @@
+import React form 'react'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Compenent {
+	render() {
+	<div>
+       <h1>Ja<span class="highlight">mmm</span>ing</h1>
+       <div class="App">
+    <!-- Add a SearchBar component --><SearchBar />
+            <div class="App-playlist">
+        <!-- Add a SearchResults component --><SearchResults />
+        <!-- Add a Playlist component --><Playlist />
+            </div>
+       </div>
+    </div>
+	}
+}
+export default App;
